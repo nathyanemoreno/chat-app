50e36f53fdea8300c52192f26146150126fac9d5
Nathyane Moreno <nathyane.moreno@gmail.com>
2024-04-11 16:34:17 -0300

    feat(api): add auth sign-up controller to router

--- apps/api/src/server/router.ts
+++ apps/api/src/server/router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { authSignInController } from "~/server/controllers/auth/sign-in";
+import { authSignUpController } from "./controllers/auth/sign-up";
 
 const router: Router = Router();
 
 // Authentication routes
 router.post("/auth/sign-in", authSignInController);
+router.post("/auth/sign-up", authSignUpController);
 
 export { router };
