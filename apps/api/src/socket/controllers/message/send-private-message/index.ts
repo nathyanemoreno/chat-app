88c056add88cdb784d8c0357ff24cd38b95900bf
Nathyane Moreno <nathyane.moreno@gmail.com>
2024-04-30 19:04:07 -0300

    feat(api): create send-multiple-message controller

--- apps/api/src/socket/controllers/message/send-private-message/index.ts
+++ apps/api/src/socket/controllers/message/send-private-message/index.ts
@@ -21,5 +21,30 @@ const sendPrivateMessage = (io: IOServer, socket: Socket, data: Message) => {
   }
 };
 
-export { sendPrivateMessage };
+const sendMultiplePrivateMessage = (
+  io: IOServer,
+  socket: Socket,
+  data: Message,
+) => {
+  const user = socket.data.user as User;
+  log(
+    `User: ${user.nickname} sent a private message to users ${JSON.stringify(data.recipients)}`,
+  );
 
+  const validatedData = validateMessageData(data);
+
+  if (typeof validatedData.recipients !== "string") {
+    // TODO: send message to unavailable users
+    const recipientsId = validatedData.recipients.map(
+      (id) => userSocketMap[id]?.socketId,
+    );
+    log("sendMultiplePrivateMessage");
+
+    if (recipientsId)
+      io.to([socket.id, ...recipientsId]).emit("message:emit", {
+        text: data.text,
+      } as MessageContent);
+  }
+};
+
+export { sendPrivateMessage, sendMultiplePrivateMessage };
