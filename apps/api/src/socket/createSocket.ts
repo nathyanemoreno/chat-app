bb7f9ef4117ed995ce839e1578bf697d5a2b3d23
Nathyane Moreno <nathyane.moreno@gmail.com>
2024-04-30 18:34:13 -0300

    feat(api): integrate socket auth-middleware

--- apps/api/src/socket/createSocket.ts
+++ apps/api/src/socket/createSocket.ts
@@ -1,6 +1,17 @@
 import { log } from "@repo/logger";
 import { Server } from "http";
 import { Server as IOServer, Socket } from "socket.io";
+import { authMiddleware } from "./middlewares/auth-middleware";
+
+ //Username is unique
+export type UserSocketMap = {
+  [username: string]: {
+    socketId: string;
+    socket?: Socket;
+  };
+};
+
+export const userSocketMap:UserSocketMap = {};
 
 const onConnection = (io: IOServer, socket: Socket) => {
   log("initial transport " + socket.conn.transport.name); // prints "polling"
@@ -23,6 +34,8 @@ const createSocket = (server: Server) => {
       origin: "*",
     },
   });
+
+  io.use(authMiddleware);
 
   io.on("connection", (socket: Socket) => onConnection(io, socket));
 
