6b0bedd0bce39df0abb1f2415ac6de1d58fe5998
Nathyane Moreno <nathyane.moreno@gmail.com>
2024-05-11 17:27:23 -0300

    feat(database): create initData seed

---
+++ packages/database/prisma/seed/initData.ts
@@ -0,0 +1,82 @@
+import { PrismaClient } from "@prisma/client";
+import { ENV_VARIABLES } from "../../config/env";
+import { hashPassword } from "../utils/hash";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.groupEnrollment.deleteMany({});
+
+  await prisma.user.deleteMany({});
+
+  await prisma.group.deleteMany({});
+
+  const adminPassword = await hashPassword(
+    ENV_VARIABLES.DEFAULT_ADMIN_PASSWORD,
+  );
+  const testerPassword = await hashPassword(
+    ENV_VARIABLES.TESTER_DEFAULT_PASSWORD,
+  );
+
+  const admin = await prisma.user.upsert({
+    where: { email: "admin@nappla.com" },
+    update: {},
+    create: {
+      email: "admin@nappla.com",
+      password: adminPassword,
+      name: "admin",
+      nickname: "Administrator",
+    },
+  });
+ 
+  const allUsers = await prisma.group.create({
+    data: {
+      name: "AllUsers",
+      members: {
+        create: {
+          role: "MEMBER",
+          user: {
+            connect: {
+              email: "admin@nappla.com",
+            },
+          },
+        },
+      },
+    },
+    include: {
+      members: true,
+    },
+  });
+ 
+ const tester = await prisma.user.upsert({
+    where: { email: "tester@nappla.com" },
+    update: {},
+    create: {
+      email: "tester@nappla.com",
+      password: testerPassword,
+      name: "tester",
+      nickname: "Tester",
+      groups: {
+        create: {
+          role: "MEMBER",
+          group: {
+            connect: {
+              id: allUsers.id
+            }
+          }
+        }
+      }
+    },
+  });
+
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
---
+++ packages/database/prisma/utils/hash.ts
@@ -0,0 +1,16 @@
+import { ENV_VARIABLES } from "./../../config/env";
+import bcrypt from "bcrypt";
+
+// Function to hash a password using bcrypt with the retrieved salt rounds
+async function hashPassword(password: string): Promise<string> {
+  try {
+    const saltRounds = ENV_VARIABLES.SALT_ROUNDS;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    console.error('Error hashing password:', error);
+    throw error;
+  }
+}
+
+export {hashPassword}
